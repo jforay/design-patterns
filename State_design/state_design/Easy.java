42885c61d09da5ced4b135f0d11a3b9debc45bd2
jforay <89590401+jforay@users.noreply.github.com>
2021-11-19 10:43:57 -0500

    Incomplete Work

---
+++ State_design/state_design/Easy.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Easy implements State {
+    private ArithemeticGame game;
+    public int getNum(){
+        Random rand = new Random();
+        int num = rand.nextInt(10);
+        return num;
+    }
+    public String getOperation(){
+        Random rand = new Random();
+        int num = rand.nextInt(2);
+        if(num == 0){
+            return " + ";
+        }
+        else
+            return " - "; 
+        
+    }
+    public void levelUp(){
+        this.game.setState(game.getMediumState());
+        System.out.println("You have been advanced to Medium mode.");
+    }
+    public void levelDown(){
+        System.out.println("You seem to be struggling, you may want to study.");
+    }
+}
---
+++ State_design/state_design/Medium.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Medium implements State{
+    private ArithemeticGame game;
+    public int getNum(){
+        Random rand = new Random();
+        int num = rand.nextInt(50);
+        return num;
+    }
+    public String getOperation(){
+        Random rand = new Random();
+        int num = rand.nextInt(3);
+        if(num == 0)
+            return " + ";
+        else if(num == 1)
+            return " - "; 
+        else 
+            return " * ";
+        
+    }
+    public void levelUp(){
+        this.game.setState(this.game.getHardState());
+        System.out.println("You have been advanced to the hardest mode.");
+    }
+    public void levelDown(){
+        this.game.setState(this.game.getEasyState());
+        System.out.println("You are struggling, let's go to easy mode.");
+    }
+}
